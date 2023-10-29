b9eefb6c989513b80878d620e5ba88241c58bc5e
Shepherd <ismailillias34@gmail.com>
2023-10-29 21:53:11 +0000

    "adding"

--- scripts/greetings.c
+++ scripts/greetings.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <time.h>
+
 
 int main(int argc, char const *argv[])
 {
-    if (argc <2)
-    puts("Hello , you handsome beast!");
-    else
-        printf("Hello, %s!\n", argv[1]);
+    time_t now;
+
+    time(&now);
+    //if (argc <2)
+    //puts("Hello , you handsome beast!");
+    //else
+    //    printf("Hello, %s!\n", argv[1]);
+    printf("The pc thinks it's %ld\n",now);
+    printf("%s",ctime(&now));
     return 0;
 }
